bd7c51e73ec2f6e78cdc66c78c81a992f4cf13d7
Alexander Evstratov <alexdrugev@me.com>
2024-06-19 13:13:19 +0300

    Create project, add sprites.

---
+++ Assets/_Scripts/MenuController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    private void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        Time.timeScale = 1;
+    }
+}
